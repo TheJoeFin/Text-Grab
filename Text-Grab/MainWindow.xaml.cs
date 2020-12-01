689ea3ce237bd112546c325f7846deeff7056397
Joseph Finney <josephfinney@LIVE.COM>
2020-12-01 06:18:26 -0600

    Add project files.

---
+++ Text-Grab/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Text_Grab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScreenshotBTN_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Point windowPoint = this.PointToScreen(new System.Windows.Point(0, 0));
+            
+            Rectangle rect = new Rectangle((int)windowPoint.X, (int)windowPoint.Y, (int)this.ActualWidth, (int)this.ActualHeight);
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            // bmp.Save(fileName, ImageFormat.Jpeg);
+            ScreenshotImage.Source = BitmapToImageSource(bmp);
+
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
