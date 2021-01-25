44e162af220f2fcd8c8f94d9ba2687071639724f
Joseph Finney <josephfinney@LIVE.COM>
2021-01-24 20:44:41 -0600

    ➕ JumpLists and Settings Window

--- Text-Grab/App.xaml.cs
+++ Text-Grab/App.xaml.cs
@@ -11,54 +11,68 @@ namespace Text_Grab
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        protected override void OnActivated(EventArgs e)
+        void appStartup(object sender, StartupEventArgs e)
         {
-            base.OnActivated(e);
+            for (int i = 0; i != e.Args.Length; ++i)
+            {
+                if (e.Args[i] == "Settings")
+                {
+                    SettingsWindow sw = new SettingsWindow();
+                    sw.Show();
+                }
+            }
+
+            if(e.Args.Length == 0)
+                NormalLaunch();
+        }
+        
+        protected void NormalLaunch()
+        {
+            // base.OnActivated(e);
 
             var allScreens = Screen.AllScreens;
             var allWindows = System.Windows.Application.Current.Windows;
 
-            if (allScreens.Count() > 1)
+            foreach (Screen screen in allScreens)
             {
-                foreach (Screen screen in allScreens)
+                bool screenHasWindow = true;
+
+                foreach (Window window in allWindows)
                 {
-                    bool screenHasWindow = true;
+                    System.Drawing.Point windowCenter = 
+                        new System.Drawing.Point(
+                            (int)(window.Left + (window.Width / 2)), 
+                            (int)(window.Top + (window.Height / 2)));
+                    screenHasWindow = screen.Bounds.Contains(windowCenter);
+                }
 
-                    foreach (Window window in allWindows)
-                    {
-                        System.Drawing.Point windowCenter = 
-                            new System.Drawing.Point(
-                                (int)(window.Left + (window.Width / 2)), 
-                                (int)(window.Top + (window.Height / 2)));
-                        screenHasWindow = screen.Bounds.Contains(windowCenter);
-                    }
+                if (allWindows.Count < 1)
+                    screenHasWindow = false;
 
-                    if(screenHasWindow == false)
-                    {
-                        MainWindow mw = new MainWindow();
-                        mw.WindowStartupLocation = WindowStartupLocation.Manual;
-                        mw.Width = 200;
-                        mw.Height = 200;
+                if(screenHasWindow == false)
+                {
+                    MainWindow mw = new MainWindow();
+                    mw.WindowStartupLocation = WindowStartupLocation.Manual;
+                    mw.Width = 200;
+                    mw.Height = 200;
 
-                        mw.WindowState = WindowState.Normal;
+                    mw.WindowState = WindowState.Normal;
 
-                        if(screen.WorkingArea.Left > 0)
-                            mw.Left = screen.WorkingArea.Left;
-                        else
-                            mw.Left = screen.WorkingArea.Left / 2;
+                    if(screen.WorkingArea.Left > 0)
+                        mw.Left = screen.WorkingArea.Left;
+                    else
+                        mw.Left = screen.WorkingArea.Left / 2;
 
-                        if(screen.WorkingArea.Top > 0)
-                            mw.Top = screen.WorkingArea.Top;
-                        else
-                            mw.Top = screen.WorkingArea.Top / 2;
+                    if(screen.WorkingArea.Top > 0)
+                        mw.Top = screen.WorkingArea.Top;
+                    else
+                        mw.Top = screen.WorkingArea.Top / 2;
 
-                        mw.Show();
-                    }
+                    mw.Show();
                 }
             }
 
-            
-            if(Settings.Default.FirstRun)
+            if (Settings.Default.FirstRun)
             {
                 FirstRunWindow frw = new FirstRunWindow();
                 frw.Show();
@@ -66,7 +80,6 @@ namespace Text_Grab
                 Settings.Default.FirstRun = false;
                 Settings.Default.Save();
             }
-
         }
     }
 }
---
+++ Text-Grab/SettingsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Text_Grab
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
