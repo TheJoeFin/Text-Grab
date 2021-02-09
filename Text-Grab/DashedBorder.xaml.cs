5697b31d3c86a6ce6cba2586aadeb6f8cff25e96
Joseph Finney <josephfinney@LIVE.COM>
2021-02-08 20:59:40 -0600

    Add a DashedBorder control

---
+++ Text-Grab/DashedBorder.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Text_Grab
+{
+    /// <summary>
+    /// Interaction logic for DashedBorder.xaml
+    /// </summary>
+    public partial class DashedBorder : UserControl
+    {
+        public DashedBorder()
+        {
+            InitializeComponent();
+        }
+    }
+}
