f62406979afb6a405540315d98570876a948934a
Joseph Finney <josephfinney@LIVE.COM>
2021-02-09 21:32:24 -0600

    Get ManipulateTextWindow buttons working

--- Text-Grab/ManipulateTextWindow.xaml.cs
+++ Text-Grab/ManipulateTextWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -29,6 +31,30 @@ namespace Text_Grab
             CopiedText = passedString;
             InitializeComponent();
             PassedTextControl.Text = CopiedText;
+        }
+
+        private void CopyCloseBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string clipboardText = PassedTextControl.Text;
+            Clipboard.SetText(clipboardText);
+            this.Close();
+        }
+
+        private void SaveBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string fileText = PassedTextControl.Text;
+
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                RestoreDirectory = true,
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, fileText);
+            }
         }
     }
 }
