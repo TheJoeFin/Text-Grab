fd50a57ea2d79636c5707f241e39fddf6083ae0c
Joseph Finney <josephfinney@LIVE.COM>
2021-02-12 19:48:01 -0600

    Add more details and link to GitHub issues

--- Text-Grab/SettingsWindow.xaml.cs
+++ Text-Grab/SettingsWindow.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Text_Grab.Properties;
 
 namespace Text_Grab
@@ -34,6 +25,14 @@ namespace Text_Grab
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://docs.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
