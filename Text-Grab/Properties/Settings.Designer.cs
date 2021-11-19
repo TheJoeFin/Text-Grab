3b1a369b4f5359515345fc9ae249280588b1205c
Joseph Finney <josephfinney@LIVE.COM>
2021-11-19 12:08:09 -0600

    Add RunInTheBackground bool setting

--- Text-Grab/Properties/Settings.Designer.cs
+++ Text-Grab/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Text_Grab.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -164,6 +164,18 @@ namespace Text_Grab.Properties {
             }
             set {
                 this["NeverAutoUseClipboard"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool RunInTheBackground {
+            get {
+                return ((bool)(this["RunInTheBackground"]));
+            }
+            set {
+                this["RunInTheBackground"] = value;
             }
         }
     }
