fb41adbc67b9012f25da357d401bcc4ad3db65b9
Joseph Finney <josephfinney@live.com>
2022-01-27 11:10:29 -0600

    Initial work on introducing tables to TextGrab

---
+++ Text-Grab/Models/ResultTable.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+using Windows.Media.Ocr;
+
+namespace Text_Grab.Models;
+
+public class ResultTable
+{
+    public List<ResultColumn> Columns { get; set; } = new();
+
+    public List<ResultRow> Rows { get; set; } = new();
+
+    private OcrResult? OcrResult { get; set; }
+
+    public ResultTable(OcrResult ocrResultParam)
+    {
+        OcrResult = ocrResultParam;
+        ParseOcrResultIntoResultTable();
+    }
+
+    private void ParseOcrResultIntoResultTable()
+    {
+        if (OcrResult is null)
+            return;
+
+        List<Rect> allBoundingRects = new();
+
+        foreach (OcrLine ocrLine in OcrResult.Lines)
+        {
+            foreach (OcrWord ocrWord in ocrLine.Words)
+            {
+                allBoundingRects.Add(ocrWord.BoundingRect);
+            }
+        }
+    }
+}
+
+
+public class ResultColumn
+{
+    public double Height { get; set; } = 0;
+
+    public int ID { get; set; } = 0;
+}
+
+public class ResultRow
+{
+    public double Width { get; set; } = 0;
+
+    public int ID { get; set; } = 0;
+}
+
+public class ResultTableCell
+{
+    public int ResultRowID { get; set; } = 0;
+    public int ResultColumnID { get; set; } = 0;
+
+    public string ResultText { get; set; } = "";
+}
---
+++ Text-Grab/Utilities/NumberExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Text_Grab;
+
+public static class Extensions
+{
+    // https://stackoverflow.com/a/2878000/7438031 
+    // Read on 1/27/2022
+    public static double StdDev(this IEnumerable<double> values)
+    {
+        // ref: http://warrenseen.com/blog/2006/03/13/how-to-calculate-standard-deviation/
+        double mean = 0.0;
+        double sum = 0.0;
+        double stdDev = 0.0;
+        int n = 0;
+        foreach (double val in values)
+        {
+            n++;
+            double delta = val - mean;
+            mean += delta / n;
+            sum += delta * (val - mean);
+        }
+        if (1 < n)
+            stdDev = Math.Sqrt(sum / (n - 1));
+
+        return stdDev;
+    }
+}
