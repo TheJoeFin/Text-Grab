5d488299023e7d83ff342e05a09293ec3aaffddb
Joseph Finney <josephfinney@live.com>
2022-02-17 14:36:02 -0600

    More results row and column info

--- Text-Grab/Models/ResultTable.cs
+++ Text-Grab/Models/ResultTable.cs
@@ -43,14 +43,22 @@ public class ResultTable
 
 public class ResultColumn
 {
-    public double Height { get; set; } = 0;
+    public double Width { get; set; } = 0;
+
+    public double Left { get; set; } = 0;
+
+    public double Right { get; set; } = 0;
 
     public int ID { get; set; } = 0;
 }
 
 public class ResultRow
 {
-    public double Width { get; set; } = 0;
+    public double Height { get; set; } = 0;
+
+    public double Top { get; set; } = 0;
+
+    public double Bottom { get; set; } = 0;
 
     public int ID { get; set; } = 0;
 }
