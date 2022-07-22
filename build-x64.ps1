5a2262ea27b0206ca09fa59ec058daca14881b15
Joseph Finney <josephfinney@live.com>
2022-07-22 14:51:11 -0500

    Build Self-contained version at the same time

--- build-x64.ps1
+++ build-x64.ps1
@@ -1,8 +1,10 @@
 $BuildPath = "$PSScriptRoot\bld\x64"
+$BuildPathSC = "$PSScriptRoot\bld\x64\Text-Grab-Self-Contained"
 $Version = Get-Date -Format "yyyy-MM-dd" # 2020-11-1
 $VersionDot = $Version -replace '-','.'
 $Project = "Text-Grab"
 $Archive = "$BuildPath\$Project-$Version.zip"
+$ArchiveSC = "$BuildPath\$Project-SC-$Version.zip"
 
 # Clean up
 if(Test-Path -Path $BuildPath)
@@ -23,5 +25,21 @@ dotnet publish "$PSScriptRoot\$Project\$Project.csproj" `
 	   -p:Version=$VersionDot `
 	   --nologo
 
-# Archiv Build
+# Archive Build
 Compress-Archive -Path "$BuildPath\$Project.exe" -DestinationPath $Archive
+
+# Dotnet restore and build
+dotnet publish "$PSScriptRoot\$Project\$Project.csproj" `
+	   --runtime win-x64 `
+	   --self-contained true `
+	   -c Release `
+	   -v minimal `
+	   -o $BuildPathSC `
+	   -p:PublishReadyToRun=true `
+	   -p:PublishSingleFile=true `
+	   -p:CopyOutputSymbolsToPublishDirectory=false `
+	   -p:Version=$VersionDot `
+	   --nologo
+
+# Archive Build
+Compress-Archive -Path "$BuildPathSC" -DestinationPath $ArchiveSC
