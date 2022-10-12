0f47a26be3964815fc73a490ebd213ba93331e21
Joseph Finney <josephfinney@live.com>
2022-10-12 11:18:50 -0500

    Add IEquatable interface

--- Text-Grab/Models/LookupItem.cs
+++ Text-Grab/Models/LookupItem.cs
@@ -1,6 +1,8 @@
-﻿namespace Text_Grab.Models;
+﻿using System;
 
-public class LookupItem
+namespace Text_Grab.Models;
+
+public class LookupItem : IEquatable<LookupItem>
 {
     public string shortValue { get; set; } = string.Empty;
     public string longValue { get; set; } = string.Empty;
@@ -19,4 +21,15 @@ public class LookupItem
     public override string ToString() => $"{shortValue} {longValue}";
 
     public string ToCSVString() => $"{shortValue},{longValue}";
+
+    public bool Equals(LookupItem? other)
+    {
+        if (other is null)
+            return false;
+
+        if (other.ToString() == ToString())
+            return true;
+
+        return false;
+    }
 }
