92cfef92e4324d468642de51c0ecb1f12dacf3c2
Joseph Finney <josephfinney@LIVE.COM>
2022-10-23 21:47:53 -0500

    Trim Toast text if it is over 5kb

--- Text-Grab/Utilities/NotificationUtilities.cs
+++ Text-Grab/Utilities/NotificationUtilities.cs
@@ -8,6 +8,17 @@ internal static class NotificationUtilities
 {
     internal static void ShowToast(string copiedText)
     {
+        int byteSizeCopiedText = System.Text.Encoding.Unicode.GetByteCount(copiedText);
+        // Max payload size is 5000, so I will go under that by a little bit
+        int maxArgumentSize = 4900;
+        if (byteSizeCopiedText > maxArgumentSize)
+        {
+            double lengthOfCopiedText = copiedText.Length;
+            double reductionRatio = maxArgumentSize / lengthOfCopiedText;
+            int newTrimmedLenth = (int)(lengthOfCopiedText * reductionRatio);
+            copiedText = copiedText.Substring(0, newTrimmedLenth);
+        }
+
         byte[] plainTextBytes = Encoding.UTF8.GetBytes(copiedText);
         string encodedString = Convert.ToHexString(plainTextBytes);
 
