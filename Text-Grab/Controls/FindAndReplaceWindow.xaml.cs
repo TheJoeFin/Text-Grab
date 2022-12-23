32027c56c13387b87a11948ae3febcc172672c63
Joseph Finney <josephfinney@LIVE.COM>
2022-12-23 12:30:44 -0500

    Replace all with multi-selection working

--- Text-Grab/Controls/FindAndReplaceWindow.xaml.cs
+++ Text-Grab/Controls/FindAndReplaceWindow.xaml.cs
@@ -113,9 +113,7 @@ public partial class FindAndReplaceWindow : Window
         Pattern = FindTextBox.Text;
 
         if (UsePaternCheckBox.IsChecked is false)
-        {
             Pattern = Pattern.EscapeSpecialRegexChars();
-        }
 
         try
         {
@@ -123,7 +121,6 @@ public partial class FindAndReplaceWindow : Window
                 Matches = Regex.Matches(StringFromWindow.ToLower(), Pattern, RegexOptions.Multiline);
             else
                 Matches = Regex.Matches(StringFromWindow.ToLower(), Pattern, RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
-
         }
         catch (Exception ex)
         {
@@ -134,8 +131,6 @@ public partial class FindAndReplaceWindow : Window
         if (Matches.Count < 1 || string.IsNullOrWhiteSpace(FindTextBox.Text))
         {
             MatchesText.Text = "0 Matches";
-            // ResultsListView.Items.Add("No Matches");
-            // ResultsListView.IsEnabled = false;
             return;
         }
 
@@ -284,7 +279,8 @@ public partial class FindAndReplaceWindow : Window
         var selection = ResultsListView.SelectedItems;
 
         if (Matches == null
-            || Matches.Count < 1)
+            || Matches.Count < 1
+            || textEditWindow is null)
             return;
 
         if (selection.Count < 2)
@@ -292,30 +288,19 @@ public partial class FindAndReplaceWindow : Window
             for (int i = Matches.Count - 1; i >= 0; i--)
             {
                 Match matchItem = Matches[i];
-                if (TextEditWindow != null)
-                {
-                    TextEditWindow.PassedTextControl.Select(matchItem.Index, matchItem.Length);
-                    TextEditWindow.PassedTextControl.SelectedText = ReplaceTextBox.Text;
-                }
+                textEditWindow.PassedTextControl.Select(matchItem.Index, matchItem.Length);
+                textEditWindow.PassedTextControl.SelectedText = ReplaceTextBox.Text;
             }
         }
         else
         {
             for (int j = selection.Count - 1; j >= 0; j--)
             {
-                string? selectionItem = selection[j] as string;
-                if (selectionItem != null && TextEditWindow != null)
-                {
-                    string? intString = selectionItem.Split('\t').FirstOrDefault();
-                    if (intString != null)
-                    {
-                        int currentIndex = int.Parse(intString);
-                        currentIndex--;
-                        Match match = Matches[currentIndex];
-                        TextEditWindow.PassedTextControl.Select(match.Index, match.Length);
-                        TextEditWindow.PassedTextControl.SelectedText = ReplaceTextBox.Text;
-                    }
-                }
+                if (selection[j] is not FindResult selectedResult)
+                    continue;
+
+                textEditWindow.PassedTextControl.Select(selectedResult.SelectionStart, selectedResult.SelectionLength);
+                textEditWindow.PassedTextControl.SelectedText = ReplaceTextBox.Text;
             }
         }
 
