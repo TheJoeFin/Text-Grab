5386b5480aa5886c368cddc3dbe0ac598a718aab
Joseph Finney <josephfinney@live.com>
2022-12-29 16:49:54 -0500

    Start building an hocr reader for tesseract

--- Text-Grab/Utilities/TesseractHelper.cs
+++ Text-Grab/Utilities/TesseractHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Text_Grab.Utilities;
@@ -8,11 +11,16 @@ namespace Text_Grab.Utilities;
 // Install Tesseract for Windows from UB-Mannheim
 // https://github.com/UB-Mannheim/tesseract/wiki
 
+// Docs about commandline usage
+// https://tesseract-ocr.github.io/tessdoc/Command-Line-Usage.html 
+
+// This was developed using Tesseract v5 in 2022
+
 public static class TesseractHelper
 {
-    public static async Task<string> GetTextFromImage(string pathToFile)
+    public static async Task<string> GetTextFromImage(string pathToFile, bool outputHocr = false)
     {
-        string rawPath = @"%LOCALAPPDATA%\Tesseract-OCR\tesseract.exe";
+        string rawPath = @"%LOCALAPPDATA%\Programs\Tesseract-OCR\tesseract.exe";
         string tesExePath = Environment.ExpandEnvironmentVariables(rawPath);
 
         if (!File.Exists(tesExePath))
@@ -21,10 +29,15 @@ public static class TesseractHelper
         if (!File.Exists(tesExePath))
             return "Cannot find tesseract.exe";
 
+        string argumentsString = $"\"{pathToFile}\" - -l eng";
+
+        if (outputHocr)
+            argumentsString += " hocr";
+
         ProcessStartInfo psi = new()
         {
             FileName = tesExePath,
-            Arguments = $"\"{pathToFile}\" - -l eng",
+            Arguments = argumentsString,
             RedirectStandardOutput = true,
             UseShellExecute = false,
             CreateNoWindow = true,
@@ -45,7 +58,7 @@ public static class TesseractHelper
         if (process.HasExited)
         {
             string returningResult = await sr.ReadToEndAsync();
-            
+
             if (!string.IsNullOrWhiteSpace(returningResult))
                 return returningResult;
 
@@ -67,5 +80,57 @@ public static class TesseractHelper
         }
 
         return $"{exePath}\\tempImage.png";
+    }
+}
+
+public class TessOcrLine
+{
+    public string Text { get; set; } = string.Empty;
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
+
+public static class HocrReader
+{
+    public static List<TessOcrLine> ReadLines(string hocrText)
+    {
+        // Create a list to hold the OcrLine objects
+        var lines = new List<TessOcrLine>();
+
+        // Split the hOCR text into lines
+        var hocrLines = hocrText.Split(new string[] { "<span class='ocr_line'", "</span>" }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Iterate through the lines
+        foreach (var hocrLineText in hocrLines)
+        {
+            // Extract the line information
+            var line = ReadLine(hocrLineText);
+
+            // Add the line to the list
+            lines.Add(line);
+        }
+
+        return lines;
+    }
+
+    private static TessOcrLine ReadLine(string hocrLineText)
+    {
+        // Create a new OcrLine object
+        TessOcrLine line = new();
+
+        // Extract the text of the line from the hOCR text
+        var textMatch = Regex.Match(hocrLineText, "<span class='ocr_line'[^>]*>(.*?)</span>");
+        line.Text = textMatch.Groups[1].Value;
+
+        // Extract the bounding box coordinates from the hOCR text
+        var bboxMatch = Regex.Match(hocrLineText, "bbox (\\d+) (\\d+) (\\d+) (\\d+)");
+        line.X = int.Parse(bboxMatch.Groups[1].Value);
+        line.Y = int.Parse(bboxMatch.Groups[2].Value);
+        line.Width = int.Parse(bboxMatch.Groups[3].Value);
+        line.Height = int.Parse(bboxMatch.Groups[4].Value);
+
+        return line;
     }
 }
