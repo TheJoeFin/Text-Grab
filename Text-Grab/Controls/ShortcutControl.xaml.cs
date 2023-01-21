cbf75c7822cf6e4f39747ec0f79850258e33a7f8
Joseph Finney <josephfinney@LIVE.COM>
2023-01-21 10:40:30 -0600

    Replace text with ShortcutControl rounded key rects

--- Text-Grab/Controls/ShortcutControl.xaml.cs
+++ Text-Grab/Controls/ShortcutControl.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace Text_Grab.Controls
+namespace Text_Grab.Controls;
+
+/// <summary>
+/// Interaction logic for ShortcutControl.xaml
+/// </summary>
+public partial class ShortcutControl : UserControl
 {
-    /// <summary>
-    /// Interaction logic for ShortcutControl.xaml
-    /// </summary>
-    public partial class ShortcutControl : UserControl
+    public ShortcutControl()
     {
-        public ShortcutControl()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
