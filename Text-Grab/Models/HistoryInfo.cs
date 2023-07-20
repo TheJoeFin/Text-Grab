73c34e9dc75bb762482432ad3e3dc7669cffc1df
Joseph Finney <josephfinney@LIVE.COM>
2023-07-19 23:19:24 -0500

    Add dpi and language on History Info

--- Text-Grab/Models/HistoryInfo.cs
+++ Text-Grab/Models/HistoryInfo.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Text.Json.Serialization;
 using System.Windows;
+using Text_Grab.Utilities;
+using Windows.Globalization;
 
 namespace Text_Grab.Models;
 
 public class HistoryInfo : IEquatable<HistoryInfo>
 {
+    #region Constructors
+
     public HistoryInfo()
     {
 
     }
+
+    #endregion Constructors
+
+    #region Properties
 
     public DateTimeOffset CaptureDateTime { get; set; }
 
@@ -22,6 +31,22 @@ public class HistoryInfo : IEquatable<HistoryInfo>
     public string ImagePath { get; set; } = string.Empty;
 
     public bool IsTable { get; set; } = false;
+
+    public double DpiScaleFactor { get; set; } = 1.0;
+
+    public string LanguageTag { get; set; } = String.Empty;
+
+    [JsonIgnore]
+    public Language OcrLanguage
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(LanguageTag))
+                return LanguageUtilities.GetCurrentInputLanguage();
+
+            return new Language(LanguageTag);
+        }
+    }
 
     [JsonIgnore]
     public Rect PositionRect
@@ -48,6 +73,20 @@ public class HistoryInfo : IEquatable<HistoryInfo>
 
     private string RectAsString { get; set; } = string.Empty;
 
+    #endregion Properties
+
+    #region Public Methods
+
+    public static bool operator !=(HistoryInfo? left, HistoryInfo? right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator ==(HistoryInfo? left, HistoryInfo? right)
+    {
+        return EqualityComparer<HistoryInfo>.Default.Equals(left, right);
+    }
+
     public bool Equals(HistoryInfo? other)
     {
         if (other is null)
@@ -58,4 +97,16 @@ public class HistoryInfo : IEquatable<HistoryInfo>
 
         return false;
     }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as HistoryInfo);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ID);
+    }
+
+    #endregion Public Methods
 }
