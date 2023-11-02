2ef5af8c0640a80086e364729f806f188afec239
Joseph Finney <TheJoeFin@users.noreply.github.com>
2023-11-01 19:51:51 -0500

    Move QR Code Logic to the QrCodeWindow and Barcode Utilities

--- Text-Grab/Controls/QrCodeWindow.xaml.cs
+++ Text-Grab/Controls/QrCodeWindow.xaml.cs
@@ -38,6 +38,11 @@ namespace Text_Grab.Controls
             SetQrCodeToText(textOfCode);
         }
 
+        private void TextDebounceTimer_Tick(object? sender, EventArgs e)
+        {
+            SetQrCodeToText(TextOfCode);
+        }
+
         private void SetQrCodeToText(string textOfCode)
         {
             TextOfCode = textOfCode;
