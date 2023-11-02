e6377d44682aad0720684fa93a0409d5881b3527
Joseph Finney <TheJoeFin@users.noreply.github.com>
2023-11-01 22:38:08 -0500

    Add SVG Saving to QR Codes

---
+++ Tests/QrCodeTests.cs
@@ -0,0 +1,15 @@
+﻿using Text_Grab.Utilities;
+
+namespace Tests;
+
+public class QrCodeTests
+{
+    [Fact]
+    public void generateSvgImage()
+    {
+        string testString = "This is only a test";
+        var svg = BarcodeUtilities.GetSvgQrCodeForText(testString);
+
+        Assert.NotNull(svg);
+    }
+}
