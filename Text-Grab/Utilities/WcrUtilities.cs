9495c5e5a8af8afdb0a19fe25ce23baf7df9dc73
Joseph Finney <josephfinney@LIVE.COM>
2025-05-31 15:09:08 -0500

    Update packages and connect to OCR

--- Text-Grab/Utilities/WcrUtilities.cs
+++ Text-Grab/Utilities/WcrUtilities.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.Graphics.Imaging;
 using Microsoft.Windows.AI;
-using Microsoft.Windows.Vision;
+using Microsoft.Windows.AI.Imaging;
+using Microsoft.Windows.Management.Deployment;
 using System;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Graphics.Imaging;
-using Windows.Storage;
-using Windows.Storage.Streams;
-
 using Text_Grab.Extensions;
+using Windows.Graphics.Imaging;
 
 
 namespace Text_Grab.Utilities;
@@ -17,12 +15,15 @@ public static class WcrUtilities
 {
     public static async Task<string> GetTextWithWcr(string imagePath)
     {
+        if (!AppUtilities.IsPackaged())
+            return "ERROR: This method requires a packaged app environment.";
+
         AIFeatureReadyState readyState = TextRecognizer.GetReadyState();
-        if (readyState is not AIFeatureReadyState.Ready and not AIFeatureReadyState.EnsureNeeded)
+        if (readyState is AIFeatureReadyState.NotSupportedOnCurrentSystem)
         {
-            return "Local model not ready please try again soon.";
+            return "ERROR: Windows AI not supported";
         }
-        if (readyState == AIFeatureReadyState.EnsureNeeded)
+        if (readyState == AIFeatureReadyState.NotReady)
         {
             AIFeatureReadyResult op = await TextRecognizer.EnsureReadyAsync();
         }
@@ -30,18 +31,21 @@ public static class WcrUtilities
         using TextRecognizer textRecognizer = await TextRecognizer.CreateAsync();
 
         SoftwareBitmap bitmap = await imagePath.FilePathToSoftwareBitmapAsync();
-        ImageBuffer imageBuffer = ImageBuffer.CreateBufferAttachedToBitmap(bitmap);
+        ImageBuffer imageBuffer = ImageBuffer.CreateForSoftwareBitmap(bitmap);
 
         // System.UnauthorizedAccessException: 'Access is denied.
+        
         RecognizedText? result = textRecognizer?
-            .RecognizeTextFromImage(imageBuffer, new TextRecognizerOptions());
+            .RecognizeTextFromImage(imageBuffer);
+
+
+        if (result == null)
+            return "ERROR: No text recognized";
 
         StringBuilder stringBuilder = new();
 
         foreach (RecognizedLine? line in result.Lines)
-        {
             stringBuilder.AppendLine(line.Text);
-        }
 
         return stringBuilder.ToString();
     }
