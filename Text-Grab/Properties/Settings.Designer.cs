ebeae9ade0a726975a64b1eff6675bfe049a64df
Joseph Finney <josephfinney@live.com>
2025-08-03 21:29:27 -0500

    Add settings related to calc pane

--- Text-Grab/Properties/Settings.Designer.cs
+++ Text-Grab/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Text_Grab.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -632,6 +632,30 @@ namespace Text_Grab.Properties {
             }
             set {
                 this["EtwShowLangPicker"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CalcShowErrors {
+            get {
+                return ((bool)(this["CalcShowErrors"]));
+            }
+            set {
+                this["CalcShowErrors"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool CalcShowPane {
+            get {
+                return ((bool)(this["CalcShowPane"]));
+            }
+            set {
+                this["CalcShowPane"] = value;
             }
         }
     }
